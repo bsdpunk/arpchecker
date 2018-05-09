1a069ac43c8bbfe24ba5b35ad9adeaed8d7969f6
Dusty <bsdpunk@gmail.com>
2018-05-09 15:37:41 -0500

    Changes

--- arpcheck.pl
+++ arpcheck.pl
@@ -1,30 +1,31 @@
-    use Net::Pcap::Easy;
-    my @internalMac = qw( 0023329dcdf5 0023120914f9 );
-    # all arguments to new are optoinal
-    my $npe = Net::Pcap::Easy->new(
-        dev              => "wlan1",
-        packets_per_loop => 10,
-        bytes_to_capture => 1024,
-        timeout_in_ms    => 0, # 0ms means forever
-        promiscuous      => 0, # true or false
-        default_callback =>          sub {
+#!/usr/bin/perl
+use Net::Pcap::Easy;
+my @internalMac = qw( 0023329dcdf5 0023120914f9 );
+# all arguments to new are optoinal
+my $npe = Net::Pcap::Easy->new(
+    dev              => "en0",
+    packets_per_loop => 10,
+    bytes_to_capture => 1024,
+    timeout_in_ms    => 0, # 0ms means forever
+    promiscuous      => 0, # true or false
+    default_callback =>          sub {
 
         my ($npe, $ether, $po, $spo) = @_;
 
         if( $po ) {
-              if( $po->isa("NetPacket::ARP") ) {
+            if( $po->isa("NetPacket::ARP") ) {                
                 print "ARP packet: $po->{sha} -> $po->{tha}\n";
                 my $element = $po->{sha};
                 print $element;
-                     if (grep {$_ eq $element} @internalMac) {
-                       print " ARP address is yours"."\n" ;
-                     }else{
-                            print " intruder!\n";
-                     }
-              }
-       
-        
+                if (grep {$_ eq $element} @internalMac) {
+                    print " ARP address is yours"."\n" ;
+                }else{
+                    print " intruder!\n";
+                }
+            }
+
+
         } 
     }
- );
-    1 while $npe->loop;
+);
+1 while $npe->loop;
